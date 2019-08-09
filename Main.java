6819de17d3997b0b1289149bb1d81ea9bc9775c8
kheptan <capitandacian@gmail.com>
2019-08-09 16:14:48 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+ 
+	static Scanner sc = new Scanner(System.in);
+	static String CONTINUE = "continue";
+	static String QUIT = "quit";
+	
+	public static void main(String[] args) {
+        swapWords();
+	}
+    
+	private static void swapWords() {
+		String s = null;
+		
+		while (true) {
+			s = sc.next();
+			if (s.contains(CONTINUE)) {
+				continue;
+			} else if (s.contains(QUIT)) {
+				System.out.println("Good Bye!!!");
+				break;
+			}
+            
+			for (int i=1; i<=s.length(); i++) {
+				System.out.print(s.charAt(s.length()-i));
+			}
+		}
+	}
+}
