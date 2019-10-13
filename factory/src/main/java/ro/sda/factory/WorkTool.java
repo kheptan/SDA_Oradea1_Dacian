1338ee753feff7279ab69e9b13de5548543a8ae1
kheptan <capitandacian@gmail.com>
2019-10-13 14:54:41 +0300

    add full

---
+++ factory/src/main/java/ro/sda/factory/WorkTool.java
@@ -0,0 +1,15 @@
+package ro.sda.factory;
+
+import ro.sda.enums.PredefinedTools;
+
+public abstract class WorkTool {
+	PredefinedTools tools;
+	
+	public void addTool(String name) {
+       	
+	}
+	
+	public PredefinedTools getTools() {
+		return tools;
+	}
+}
---
+++ factory/src/main/java/ro/sda/interfaces/Tools.java
@@ -0,0 +1,7 @@
+package ro.sda.interfaces;
+
+import ro.sda.factory.WorkTool;
+
+public interface Tools {
+	public void useTool(WorkTool work);
+}
